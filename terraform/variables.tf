0394c12f1ae3ebb05e9fa91e8c953401c7bb264a
Joebaho <josephmbatchou2@gmail.com>
2026-02-27 12:22:24 -0800

    Changing variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,24 @@
 variable "region" {
   description = "AWS region"
   type        = string
+  default = "us-west-2"
 }
 
 variable "cluster_name" {
   description = "EKS cluster name"
   type        = string
+  default = "automated-demo-cluster"
 }
 
 variable "environment" {
   description = "Deployment environment"
   type        = string
+  default = "production"
 }
 
 variable "dockerhub_username" {
   description = "Your Docker Hub username"
   type        = string
   sensitive   = true
+  default = "joebaho2"
 }
